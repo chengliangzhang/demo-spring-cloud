ba27f6b925ba9807f6ef4253fb7f6792a3b146e8
zhangchengliang <zhangchengliang@imaoding.com>
2018-09-29 00:08:16 +0800

    实现注册中心

--- application/main/java/com/demo/application/Application.java
+++ application/main/java/com/demo/application/Application.java
@@ -1,18 +1,18 @@
-package com.demo.hello;
+package com.demo.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * 深圳市卯丁技术有限公司
- * 日期: 2018/9/28
- * 类名: com.demo.hello.Hello
+ * 日期: 2018/9/27
+ * 类名: com.demo.application.controller.Application
  * 作者: 张成亮
  * 描述:
  **/
+@EnableEurekaServer
 @SpringBootApplication
-@EnableDiscoveryClient
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
--- demohello/src/main/java/com/demo/hello/controller/HelloController.java
+++ demohello/src/main/java/com/demo/hello/controller/HelloController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RestController;
  **/
 @RestController
 public class HelloController {
-
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String hello(){
         return "hello";
